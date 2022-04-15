16fe812ec43642535d62b2c97cd39cdcc8d895c5
Godwinh Lopez <work.glopez@gmail.com>
2022-04-16 00:51:35 +0800

    fix php tag

--- index.php
+++ index.php
@@ -18,10 +18,9 @@ if (have_posts()) {
     ?>
     <div class="nav-previous alignleft"><?php previous_posts_link('Older posts'); ?></div>
     <div class="nav-next alignright"><?php next_posts_link('Newer posts'); ?></div>
-    <?
+    <?php
 } else {
 	get_template_part('template-parts/content/no-content');
 }
 
 get_footer();
-
